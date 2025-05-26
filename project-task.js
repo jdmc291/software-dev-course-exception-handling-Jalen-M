19f33e68228abde8885a3e792151fa9a9ed827de
Jalen McBride <jamcbride131@gmail.com>
2025-05-25 22:38:15 -0500

    Created Try and Catch blocks to make the code run continuously instead of stopping when error occurs

--- project-task.js
+++ project-task.js
@@ -33,18 +33,62 @@ const readlineSync = require('readline-sync');
 let animals = [];
 let fees = [];
 function addAnimal(name, fee) {
-    if (!name || fee < 0) {
-        throw new Error("Invalid animal name or adoption fee!");
+
+    try 
+    {
+        if(!name)
+        {
+            throw new Error("Name for animal does not exist");
+        }
+        animals.push(name)
+
+        if(fee < 0 || !fee)
+        {
+            throw new Error("Value for fee does not exist or less than 0");
+        }
+
+        animals.push(name);
+        fees.push(fee);
     }
-    animals.push(name);
-    fees.push(fee);
+    catch
+    {
+        console.error("Please add name for animal and fee above 0");
+    }
+    // if (!name || fee < 0) {
+    //     throw new Error("Invalid animal name or adoption fee!");
+    // }
+    // animals.push(name);
+    // fees.push(fee);
 }
 function getAdoptionFee(animalName) {
+
     let index = animals.indexOf(animalName);
+
+    try
+    {
+        if(index === -1)
+        {
+            throw new Error("Given animal name does not exist");
+        }   
+
+        return fees[index];
+    }
+    
+    catch
+    {
+        console.error("Animal Not Found");
+        return 0;
+    }
+
+
+
+   /* 
+   let index = animals.indexOf(animalName);
     if (index === -1) {
         throw new Error("Animal not found in records!");
     }
-    return fees[index];
+    return fees[index]; 
+   */
 }
 // Main program
 console.log("Welcome to the Pet Shelter System");
@@ -74,10 +118,14 @@ Problems to Solve
 
 Invalid Input Errors:
   What happens if the user provides a negative adoption fee or leaves the name blank?
+  My Answer: It throws an exception and the program will stop running
+
   What happens if the user tries to find the fee for an animal that hasn’t been added?
+  My Answer: I got an "invalid email or adoption fee error and the program ends"
 
 Code Flow Problems:
   What happens if the program throws an exception? Does the rest of the code continue running?
+  My Answer: The program stops running
 
 Structured Exception Handling:
   Add try/catch blocks to handle the above errors gracefully.
